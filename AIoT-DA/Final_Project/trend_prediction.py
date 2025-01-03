7d11dab3e5e0f9f18fffe173933e6d1bda9c0746
林君瑋 <junweilin975@gmail.com>
2025-01-04 02:51:45 +0900

    Update trend_prediction.py

--- AIoT-DA/Final_Project/trend_prediction.py
+++ AIoT-DA/Final_Project/trend_prediction.py
@@ -1,29 +1,32 @@
-from prophet import Prophet
+from statsmodels.tsa.arima.model import ARIMA
 import pandas as pd
 import matplotlib.pyplot as plt
 
 def prepare_data(data):
     """清理並準備數據."""
-    data.rename(columns={"date": "ds", "value": "y"}, inplace=True)
+    data['date'] = pd.to_datetime(data['date'])
+    data.set_index('date', inplace=True)
     return data
 
-def train_prophet_model(data):
-    """訓練 Prophet 模型."""
-    model = Prophet()
-    model.fit(data)
-    return model
+def train_arima_model(data, order=(1, 1, 1)):
+    """訓練 ARIMA 模型."""
+    model = ARIMA(data['value'], order=order)
+    fit = model.fit()
+    return fit
 
-def predict_trends(model, periods=30):
+def predict_trends(fit, steps=30):
     """預測未來趨勢."""
-    future = model.make_future_dataframe(periods=periods)
-    forecast = model.predict(future)
+    forecast = fit.forecast(steps=steps)
     return forecast
 
 def plot_trends(data, forecast):
     """繪製趨勢圖."""
     plt.figure(figsize=(10, 6))
-    plt.plot(data['ds'], data['y'], label="歷史數據")
-    plt.plot(forecast['ds'], forecast['yhat'], label="預測數據")
-    plt.fill_between(forecast['ds'], forecast['yhat_lower'], forecast['yhat_upper'], color='gray', alpha=0.2)
+    plt.plot(data.index, data['value'], label="歷史數據")
+    forecast_index = pd.date_range(data.index[-1], periods=len(forecast)+1, freq="D")[1:]
+    plt.plot(forecast_index, forecast, label="預測數據")
     plt.legend()
+    plt.title("趨勢預測")
+    plt.xlabel("日期")
+    plt.ylabel("值")
     plt.show()
